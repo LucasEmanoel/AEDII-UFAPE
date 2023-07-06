35af976ea1b736084bdc03737e30c7a8974e1900
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-07-05 21:02:17 -0300

    FEAT: MVP - avl tree - pass in some cases

--- avl-tree/avl.c
+++ avl-tree/avl.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//testar
 tree rsd(tree node) {
 	tree p, u, t1, t2, t3;
 	p = node;
@@ -25,7 +24,6 @@ tree rsd(tree node) {
 
 	return u;
 }
-//ok
 tree rse(tree node) {
 
 	tree p, u, t1, t2, t3;
@@ -50,7 +48,6 @@ tree rse(tree node) {
 
 	return u;
 }
-
 tree rdd(tree node) {
 	tree p, u, v, t1, t2, t3, t4;
 
@@ -87,7 +84,6 @@ tree rdd(tree node) {
 
 	return v;
 }
-
 tree rde(tree node) {
 	tree p, u, v, t1, t2, t3, t4;
 
@@ -123,7 +119,6 @@ tree rde(tree node) {
 	}
 	return v;
 }
-
 tree rotatation(tree node) {
 	if (node->fb > 0) {
 		switch (node->right->fb) {
@@ -156,7 +151,6 @@ tree rotatation(tree node) {
 	}
 	return node;
 }
-
 tree insert_avl(tree node, int val, int *grown) {
 	if (node == NULL) {
 		tree new_node = (tree) malloc(sizeof(struct no));
@@ -211,7 +205,6 @@ tree insert_avl(tree node, int val, int *grown) {
 		return node;
 	}
 }
-
 int max_value(tree node) {
 	if (node == NULL) {
 		return -1;
@@ -222,7 +215,6 @@ int max_value(tree node) {
 		return max_value(node->right);
 	}
 }
-
 tree remove_avl(tree node, int val, int *reduce) {
 
 	if (node == NULL) {
@@ -289,7 +281,6 @@ tree remove_avl(tree node, int val, int *reduce) {
 	}
 	return node;
 }
-
 void switch_nodes(tree node, int val) {
 	tree aux = node->left;
 	while (aux->right != NULL) {
@@ -298,148 +289,10 @@ void switch_nodes(tree node, int val) {
 	node->val = aux->val;
 	aux->val = val;
 }
-
 void pre_order_avl(tree node) {
 	if (node != NULL) {
-		printf("[%d, %d]", node->val, node->fb);
+		printf("[%d %d]", node->val, node->fb);
 		pre_order_avl(node->left);
 		pre_order_avl(node->right);
-	}
-}
-
-void in_order_avl(tree node) {
-	if (node != NULL) {
-		in_order_avl(node->left);
-		printf("[%d]", node->val);
-		in_order_avl(node->right);
-	}
-}
-
-void pos_order_avl(tree node) {
-	if (node != NULL) {
-		pos_order_avl(node->left);
-		pos_order_avl(node->right);
-		printf("[%d]", node->val);
-	}
-}
-
-void reverse_avl(tree node) {
-	if (node != NULL) {
-		reverse_avl(node->right);
-		printf("[%d]", node->val);
-		reverse_avl(node->left);
-	}
-}
-
-int isPrime(int num) {
-	for (int i = 2; i < num; i++) {
-		if (num % i == 0)
-			return 1; // nao e primo.
-	}
-	return 0; // e primo
-}
-
-int sum_prime_numbers_avl(tree no) {
-	int aux, i;
-	if (no != NULL) {
-		aux = sum_prime_numbers_avl(no->left)
-				+ sum_prime_numbers_avl(no->right);
-		if (isPrime(no->val) == 0) {
-			return aux + 1;
-		} else {
-			return aux;
-		}
-	}
-}
-
-int successor_avl(tree node, int suc, int val) {
-	if (node != NULL) {
-		if (node->val > val && node->left == NULL) {
-			return node->val;
-		}
-		if (node->val <= val) {
-			successor_avl(node->right, suc, val);
-		} else {
-			suc = node->val;
-			successor_avl(node->left, suc, val);
-		}
-	} else {
-		return suc;
-	}
-}
-
-void path_avl(tree node, int val) {
-	if (node != NULL && node->val != val) {
-		printf("[%d]", node->val);
-		if (node->val <= val) {
-			path_avl(node->right, val);
-		} else {
-			path_avl(node->left, val);
-		}
-	}
-}
-
-int sum_all_numbers_avl(tree node) {
-	int aux;
-	if (node != NULL) {
-		aux = sum_all_numbers_avl(node->left)
-				+ sum_all_numbers_avl(node->right);
-		return aux + node->val;
-	}
-}
-
-int exist_avl(tree node, int val) {
-	if (node != NULL) {
-		if (node->val == val) {
-			return 1;
-		}
-		if (node->val <= val) {
-			exist_avl(node->right, val);
-		} else {
-			exist_avl(node->left, val);
-		}
-	} else {
-		return 0;
-	}
-}
-
-int transform_percentage(int num, float porcentagem) {
-	return num + (num * (porcentagem / 100));
-}
-
-int adjust_percentage_avl(tree node, float porcentagem) {
-	if (node != NULL) {
-		adjust_percentage_avl(node->left, porcentagem);
-		adjust_percentage_avl(node->right, porcentagem);
-		node->val = transform_percentage(node->val, porcentagem);
-	}
-}
-
-int height_avl(tree node) {
-	int left_height, right_height;
-	if (node != NULL) {
-		left_height = height_avl(node->left);
-		right_height = height_avl(node->right);
-
-		if (right_height >= left_height) {
-			return right_height + 1;
-		} else {
-			return left_height + 1;
-		}
-	} else {
-		return 0;
-	}
-}
-
-void succession_avl(tree node, int n) {
-	if (node != NULL) {
-		if (node->val == n) {
-			in_order_avl(node);
-		}
-		if (node->val <= n) {
-			succession_avl(node->right, n);
-		} else {
-			succession_avl(node->left, n);
-		}
 	}
 }
--- avl-tree/main.c
+++ avl-tree/main.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
   tree avl_tree;
-  int option, val, grown, reduce =0;
+  int option, val, grown, reduce =1;
   int * grown_aux = &grown;
   int * reduce_aux = &reduce;
   avl_tree = NULL;
@@ -22,59 +22,8 @@ int main(int argc, char *argv[])
       printf("\n");
       break;
     case 3:
-      in_order_avl(avl_tree);
-      printf("\n");
-      break;
-    case 4:
-      pos_order_avl(avl_tree);
-      printf("\n");
-      break;
-    case 5:
-      reverse_avl(avl_tree);
-      printf("\n");
-      break;
-    case 6:
-      printf("%d \n", sum_prime_numbers_avl(avl_tree));
-      break;
-    case 7:
-      scanf("%d", &val);
-      printf("%d \n", successor_avl(avl_tree, -1, val));
-      break;
-    case 8:
-      scanf("%d", &val);
-      path_avl(avl_tree, val);
-      printf("\n");
-      break;
-    case 9:
       scanf("%d", &val);
       avl_tree = remove_avl(avl_tree, val, reduce_aux);
-      printf("\n");
-      break;
-    case 10:
-      printf("%d \n", sum_all_numbers_avl(avl_tree));
-      break;
-    case 11:
-      scanf("%d", &val);
-      //prune_avl(avl_tree, val);
-      printf("\n");
-      break;
-    case 12:
-      scanf("%d", &val);
-      adjust_percentage_avl(avl_tree, val);
-      printf("\n");
-      break;
-    case 13:
-      scanf("%d", &val);
-      printf("%d \n", exist_avl(avl_tree, val));
-      break;
-    case 14:
-      scanf("%d", &val);
-      succession_avl(avl_tree, val);
-      printf("\n");
-      break;
-    case 15:
-      scanf("%d", &val);
-      printf("%d \n", height_avl(avl_tree));
       break;
     case 99:
       exit(0);
